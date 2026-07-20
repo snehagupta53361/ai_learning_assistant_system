13987f720ed9581c7204f4b0f10d92b634e69ed9
Sneha Gupta <snehagupta53361@gmail.com>
2026-07-20 19:29:27 +0530

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
           <Route path="/documents" element={<DocumentListPage />} />
           <Route path="documents/:id" element={<DocumentsDetailPage />} />
           <Route path="/flashcards" element={<FlashcardListPage />} />
-          <Route path="/documents/:id/flashcards" element={<FlashcardPage />} />
+          <Route path="/documents/:id/flashcards" element={FlashcardPage} />
           <Route path="/quizzes/:quizId" element={QuizTakePage} />
           <Route path="/quizzes/:quizId/results" element={QuizResultPage} />
           <Route path="profile" element={ProfilePage} />
